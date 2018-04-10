5460e13617802d582c4d4bed47cc0803593a969f
Ideawin-Bunthy Koun <ideawinkoun@gmail.com>
2018-04-10 02:25:11 -0400

    Copy from previous laravel project

---
+++ test2/resources/views/projects-create.blade.php
@@ -0,0 +1,80 @@
+@extends('projects-base')
+
+@section('action-content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Add new project</div>
+                    <div class="panel-body">
+                        {{--
+                        <form class="form-horizontal" role="form" method="POST" action="{{ route('SOMETHINGHERE', ['id' => $project->id]) }}" enctype="multipart/form-data">
+                        --}}
+                        <form class="form-horizontal" role="form" method="POST" action="{{route('createProject')}}" enctype="multipart/form-data">
+                            {{ csrf_field() }}
+                            <div class="form-group{{ $errors->has('id') ? ' has-error' : '' }}">
+                                <label class="col-md-4 control-label">Id</label>
+
+                                <div class="col-md-6">
+                                    <input id="id" type="text" class="form-control" name="id" value="{{ old('id') }}" required>
+
+                                    @if ($errors->has('id'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('id') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="form-group{{ $errors->has('id') ? ' has-error' : '' }}">
+                                <label class="col-md-4 control-label">Department</label>
+
+                                <div class="col-md-6">
+                                    <input id="id" type="text" class="form-control" name="id" value="{{ old('id') }}" required>
+
+                                    @if ($errors->has('id'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('id') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                                <label for="name" class="col-md-4 control-label">Name</label>
+
+                                <div class="col-md-6">
+                                    <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+
+                                    @if ($errors->has('name'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="form-group{{ $errors->has('location') ? ' has-error' : '' }}">
+                                <label for="location" class="col-md-4 control-label">Location</label>
+
+                                <div class="col-md-6">
+                                    <input id="location" type="text" class="form-control" name="location" value="{{ old('location') }}" required autofocus>
+
+                                    @if ($errors->has('location'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('location') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Create new project
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
